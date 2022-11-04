bf326874f93e40a68d52e4c08094085aa35cd8a2
kyle arthur <kylejarthur2000@gmail.com>
2022-11-04 11:45:02 -0700

    Initial commit

---
+++ For-Loop/for-loop-one.js
@@ -0,0 +1,4 @@
+for(var i = 0; 1 < 3; i++) {
+    console.log(i);
+}
+console.log("finished");
---
+++ For-Loop/for-of-loop.js
@@ -0,0 +1,12 @@
+var weather = ["sunny", "rainy", "cloudy"];
+var words = "";
+
+for(var weather of weather){
+    if(weather == cloudy) {
+    words += weather;
+    break;
+}
+words += weather + "\n";
+}
+
+console.log(words);
